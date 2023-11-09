000c43198f57c8b3c7a9b504add6992c678bd4fd
hanyswana <146700525+hanyswana@users.noreply.github.com>
2023-11-09 10:03:46 +0800

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -14,10 +14,21 @@ def load_model(model_file):
 st.title('REVA (Hb Prediction)')
 
 # Load the sample
-sample = pd.read_csv('reva-lablink-oridata-20.csv')
+# sample = pd.read_csv('reva-lablink-oridata-20.csv')
 # sample = pickle.load(open('reva-lablink-oridata.pkl', 'rb'))
 # st.write('Spectral Data:')
 # st.write(sample)
+
+with open(reva-lablink-oridata-124.json') as f1:
+    data1 = json.load(f1)
+
+with open(example1.json') as f2:
+    data2 = json.load(f2)
+
+merged_data = data1 + data2
+
+with open(merged_data.json', 'w') as f3:
+    json.dump(merged_data, f3)
 
 lr_model = load_model('pipeline  6.csv_lr_ori.joblib')
 dtr_model = load_model('pipeline  64.csv_dtr_ori.joblib')
@@ -27,8 +38,8 @@ lr_llc_model = load_model('pipeline  78.csv_lr_llc.joblib')
 dtr_llc_model = load_model('pipeline  92.csv_dtr_llc.joblib')
 
 # Apply dimension reduction to the sample using Isomap and LLC
-sample_iso = load_model('pipeline  104.csv_iso.joblib').fit_transform(sample)
-sample_llc = load_model('pipeline  50.csv_llc.joblib').fit_transform(sample)
+sample_iso = load_model('pipeline  104.csv_iso.joblib').fit_transform(merged_data)
+sample_llc = load_model('pipeline  50.csv_llc.joblib').fit_transform(merged_data)
 
 # Streamlit UI elements
 st.write('Click the "Predict" button to make prediction of Hb.')
@@ -36,8 +47,8 @@ st.write('Click the "Predict" button to make prediction of Hb.')
 # Add button to trigger prediction
 if st.button('Predict'):
     if len(sample) > 0:
-        lr_prediction = lr_model.predict(sample)
-        dtr_prediction = dtr_model.predict(sample)
+        lr_prediction = lr_model.predict(merged_data)
+        dtr_prediction = dtr_model.predict(merged_data)
         lr_iso_prediction = lr_iso_model.predict(sample_iso)
         dtr_iso_prediction = dtr_iso_model.predict(sample_iso)
         lr_llc_prediction = lr_llc_model.predict(sample_llc)
