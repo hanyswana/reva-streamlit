7f23b4d6ba28562f2e8f3169237ba15a01e5e116
hanyswana <146700525+hanyswana@users.noreply.github.com>
2023-11-09 11:16:31 +0800

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -3,7 +3,7 @@ import streamlit as st
 import joblib
 
 
-# Load a model from the joblib file
+# Load a model from the pickle file
 def load_model(model_file):
     with open(model_file, 'rb') as f:
         model = joblib.load(f)
@@ -13,22 +13,20 @@ def load_model(model_file):
 # Streamlit UI elements
 st.title('REVA (Hb Prediction)')
 
-# Load the sample
-sample = pd.read_csv('reva-lablink-oridata-20.csv')
-# sample = pickle.load(open('reva-lablink-oridata.pkl', 'rb'))
+# Load the ori data (124 samples)
+ori_data = pd.read_csv('reva-lablink-oridata-40.csv')
+
+# Load the new data (1 sample) and convert json to csv file
+json_data = pd.read_json('example1.json')
+json_data.to_csv('example1.csv', index=False)
+new_data = pd.read_csv('example1.csv')
+
+# Combine the ori data with the new data
+sample_data = pd.concat([new_data, ori_data])
+
 # st.write('Spectral Data:')
-# st.write(sample)
-
-# with open(reva-lablink-oridata-124.json') as f1:
-#     data1 = json.load(f1)
-
-# with open(example1.json') as f2:
-#     data2 = json.load(f2)
-
-# merged_data = data1 + data2
-
-# with open(merged_data.json', 'w') as f3:
-#     json.dump(merged_data, f3)
+# st.write(example)
+# st.write(merged_data)
 
 lr_model = load_model('pipeline  6.csv_lr_ori.joblib')
 dtr_model = load_model('pipeline  64.csv_dtr_ori.joblib')
@@ -38,17 +36,17 @@ lr_llc_model = load_model('pipeline  78.csv_lr_llc.joblib')
 dtr_llc_model = load_model('pipeline  92.csv_dtr_llc.joblib')
 
 # Apply dimension reduction to the sample using Isomap and LLC
-sample_iso = load_model('pipeline  104.csv_iso.joblib').fit_transform(sample)
-sample_llc = load_model('pipeline  50.csv_llc.joblib').fit_transform(sample)
+sample_iso = load_model('pipeline  104.csv_iso.joblib').fit_transform(sample_data)
+sample_llc = load_model('pipeline  50.csv_llc.joblib').fit_transform(sample_data)
 
 # Streamlit UI elements
 st.write('Click the "Predict" button to make prediction of Hb.')
 
 # Add button to trigger prediction
 if st.button('Predict'):
-    if len(sample) > 0:
-        lr_prediction = lr_model.predict(sample)
-        dtr_prediction = dtr_model.predict(sample)
+    if len(sample_data) > 0:
+        lr_prediction = lr_model.predict(sample_data)
+        dtr_prediction = dtr_model.predict(sample_data)
         lr_iso_prediction = lr_iso_model.predict(sample_iso)
         dtr_iso_prediction = dtr_iso_model.predict(sample_iso)
         lr_llc_prediction = lr_llc_model.predict(sample_llc)
