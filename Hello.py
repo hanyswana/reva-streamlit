13f72bc14d9d2d82549847184f53f1e61004e210
hanyswana <146700525+hanyswana@users.noreply.github.com>
2023-11-29 11:35:23 +0800

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -5,20 +5,6 @@ from flask import request
 import requests
 
 
-# Load a model from the pickle file
-def load_model(model_file):
-    with open(model_file, 'rb') as f:
-        model = joblib.load(f)
-    return model
-
-lr_model = load_model('pipeline  6.csv_lr_ori.joblib')
-dtr_model = load_model('pipeline  64.csv_dtr_ori.joblib')
-lr_iso_model = load_model('pipeline  85.csv_lr_iso.joblib')
-dtr_iso_model = load_model('pipeline  63.csv_dtr_iso.joblib')
-lr_llc_model = load_model('pipeline  78.csv_lr_llc.joblib')
-dtr_llc_model = load_model('pipeline  92.csv_dtr_llc.joblib')
-
-
 def json_data():
     
     # Define the API URL and payload
@@ -38,64 +24,80 @@ def json_data():
         # Display the data in the Streamlit app
         st.write('JSON data:')
         st.write(df)
-      
-        # Streamlit UI elements
-        st.title('REVA (Hb Prediction)')
 
-        # Load the ori data (124 samples)
-        ori_data = pd.read_csv('reva-lablink-oridata-124-x.csv')
-
-        # # Load the new data (1 sample) and convert json to csv file
-        # json_data = pd.read_json('data.json')
-        # json_data.to_csv('data.csv', index=False)
-        new_data = pd.read_csv('json_data.csv')
-
-        # Combine the ori data with the new data
-        sample_data = pd.concat([new_data, ori_data])
-
-        st.write('Spectral Data:')
-        st.write(sample_data)
-
-        # Apply dimension reduction to the sample using Isomap and LLC
-        sample_iso = load_model('pipeline  104.csv_iso.joblib').fit_transform(sample_data)
-        sample_llc = load_model('pipeline  50.csv_llc.joblib').fit_transform(sample_data)
-
-        # Streamlit UI elements
-        st.write('Click the "Predict" button to make prediction of Hb.')
-
-        # Add button to trigger prediction
-        if st.button('Predict'):
-            if len(sample_data) > 0:
-                lr_prediction = lr_model.predict(sample_data)
-                dtr_prediction = dtr_model.predict(sample_data)
-                lr_iso_prediction = lr_iso_model.predict(sample_iso)
-                dtr_iso_prediction = dtr_iso_model.predict(sample_iso)
-                lr_llc_prediction = lr_llc_model.predict(sample_llc)
-                dtr_llc_prediction = dtr_llc_model.predict(sample_llc)
-
-                st.markdown(f"""
-                    <style>
-                        .hb_prediction {{
-                            font-size: 18px;
-                            font-weight: bold;
-                        }}
-                    </style>
-                    <p class="hb_prediction">Hb value (LR): {lr_prediction[0]} g/dL</p>
-                    <p class="hb_prediction">Hb value (DTR): {dtr_prediction[0]:.1f} g/dL</p>
-                    <p class="hb_prediction">Hb value (LR-ISOMAP): {lr_iso_prediction[0]:.1f} g/dL</p>
-                    <p class="hb_prediction">Hb value (DTR-ISOMAP): {dtr_iso_prediction[0]:.1f} g/dL</p>
-                    <p class="hb_prediction">Hb value (LR-LLC): {lr_llc_prediction[0]:.1f} g/dL</p>
-                    <p class="hb_prediction">Hb value (DTR-LLC): {dtr_llc_prediction[0]:.1f} g/dL</p>
-                """, unsafe_allow_html=True)
-            else:
-                st.write('The sample is empty. Please load a sample with data.')
-            
         return df
     else:
         # Display an error message
         st.write("Error:", response.status_code)
         return None
 
+
+
+
+# Streamlit UI elements
+st.title('REVA (Hb Prediction)')
+
+# Load the ori data (124 samples)
+ori_data = pd.read_csv('reva-lablink-oridata-124-x.csv')
+
+# Load the new data (1 sample) and convert json to csv file
+# json_data = pd.read_json('example1.json')
+# json_data.to_csv('example1.csv', index=False)
+new_data = pd.read_csv('json_data.csv')
+
+# Combine the ori data with the new data
+sample_data = pd.concat([new_data, ori_data])
+
+# st.write('Spectral Data:')
+# st.write(example)
+# st.write(merged_data)
+
+# Load a model from the pickle file
+def load_model(model_file):
+    with open(model_file, 'rb') as f:
+        model = joblib.load(f)
+    return model
+    
+lr_model = load_model('pipeline  6.csv_lr_ori.joblib')
+dtr_model = load_model('pipeline  64.csv_dtr_ori.joblib')
+lr_iso_model = load_model('pipeline  85.csv_lr_iso.joblib')
+dtr_iso_model = load_model('pipeline  63.csv_dtr_iso.joblib')
+lr_llc_model = load_model('pipeline  78.csv_lr_llc.joblib')
+dtr_llc_model = load_model('pipeline  92.csv_dtr_llc.joblib')
+
+# Apply dimension reduction to the sample using Isomap and LLC
+sample_iso = load_model('pipeline  104.csv_iso.joblib').fit_transform(sample_data)
+sample_llc = load_model('pipeline  50.csv_llc.joblib').fit_transform(sample_data)
+
+# Streamlit UI elements
+st.write('Click the "Predict" button to make prediction of Hb.')
+
+# Add button to trigger prediction
+if st.button('Predict'):
+    if len(sample_data) > 0:
+        lr_prediction = lr_model.predict(sample_data)
+        dtr_prediction = dtr_model.predict(sample_data)
+        lr_iso_prediction = lr_iso_model.predict(sample_iso)
+        dtr_iso_prediction = dtr_iso_model.predict(sample_iso)
+        lr_llc_prediction = lr_llc_model.predict(sample_llc)
+        dtr_llc_prediction = dtr_llc_model.predict(sample_llc)
+
+        st.markdown(f"""
+            <style>
+                .hb_prediction {{
+                    font-size: 18px;
+                    font-weight: bold;
+                }}
+            </style>
+            <p class="hb_prediction">Hb value (LR): {lr_prediction[0]} g/dL</p>
+            <p class="hb_prediction">Hb value (DTR): {dtr_prediction[0]:.1f} g/dL</p>
+            <p class="hb_prediction">Hb value (LR-ISOMAP): {lr_iso_prediction[0]:.1f} g/dL</p>
+            <p class="hb_prediction">Hb value (DTR-ISOMAP): {dtr_iso_prediction[0]:.1f} g/dL</p>
+            <p class="hb_prediction">Hb value (LR-LLC): {lr_llc_prediction[0]:.1f} g/dL</p>
+            <p class="hb_prediction">Hb value (DTR-LLC): {dtr_llc_prediction[0]:.1f} g/dL</p>
+        """, unsafe_allow_html=True)
+    else:
+        st.write('The sample is empty. Please load a sample with data.')
 
 
 
