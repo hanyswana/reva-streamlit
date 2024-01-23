25509d1d75c389acd398deaad13288ce5f75184b
hanyswana <146700525+hanyswana@users.noreply.github.com>
2024-01-23 15:54:07 +0800

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -20,8 +20,8 @@ def json_data():
 
         # Convert json to csv
         df = pd.DataFrame(data)
-        df.to_csv('json_data.csv', index=False)
-        return df
+        df.iloc[:1].to_csv('json_data.csv', index=False)
+        return df.iloc[:1]
     else:
         # Display an error message
         st.write("Error:", response.status_code)
