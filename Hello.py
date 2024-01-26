16280743fb430f7efd7bf58d3f9d399ec455a04f
hanyswana <146700525+hanyswana@users.noreply.github.com>
2024-01-26 10:05:22 +0800

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -44,11 +44,11 @@ def load_model(model_file):
     return model
     
 lr_model = load_model('pipeline  6.csv_lr_ori.joblib')
-# dtr_model = load_model('pipeline  64.csv_dtr_ori.joblib')
+dtr_model = load_model('pipeline  64.csv_dtr_ori.joblib')
 lr_iso_model = load_model('pipeline  85.csv_lr_iso.joblib')
-# dtr_iso_model = load_model('pipeline  63.csv_dtr_iso.joblib')
+dtr_iso_model = load_model('pipeline  63.csv_dtr_iso.joblib')
 lr_llc_model = load_model('pipeline  78.csv_lr_llc.joblib')
-# dtr_llc_model = load_model('pipeline  92.csv_dtr_llc.joblib')
+dtr_llc_model = load_model('pipeline  92.csv_dtr_llc.joblib')
 
 
 # Streamlit UI elements
@@ -56,18 +56,18 @@ st.title('Prediction')
 
 # Load the new data (1 sample) 
 new_data = pd.read_csv('json_data.csv')
-st.write('Xano data:')
+st.write('Spectral data:')
 st.write(new_data)
 
 # Load the ori data (124 samples)
 ori_data = pd.read_csv('reva-lablink-oridata-124-x.csv')
-st.write('Ori Data:')
-st.write(ori_data)
+# st.write('Ori Data:')
+# st.write(ori_data)
 
 # Combine the ori data with the new data
 sample_data = pd.concat([new_data.iloc[:1], ori_data])
-st.write('Sample Data:')
-st.write(sample_data)
+# st.write('Sample Data:')
+# st.write(sample_data)
 
 # Apply dimension reduction to the sample using Isomap and LLC
 sample_iso = load_model('pipeline  104.csv_iso.joblib').fit_transform(sample_data)
@@ -76,11 +76,11 @@ sample_llc = load_model('pipeline  50.csv_llc.joblib').fit_transform(sample_data
 
 if len(sample_data) > 0:
     lr_prediction = lr_model.predict(sample_data)
-    # dtr_prediction = dtr_model.predict(sample_data)
+    dtr_prediction = dtr_model.predict(sample_data)
     lr_iso_prediction = lr_iso_model.predict(sample_iso)
-    # dtr_iso_prediction = dtr_iso_model.predict(sample_iso)
+    dtr_iso_prediction = dtr_iso_model.predict(sample_iso)
     lr_llc_prediction = lr_llc_model.predict(sample_llc)
-    # dtr_llc_prediction = dtr_llc_model.predict(sample_llc)
+    dtr_llc_prediction = dtr_llc_model.predict(sample_llc)
 
     st.markdown(f"""
         <style>
@@ -90,8 +90,11 @@ if len(sample_data) > 0:
             }}
         </style>
         <p class="hb_prediction">Hb value (LR): {lr_prediction[0]} g/dL</p>
-        <p class="hb_prediction">Hb value (LR-ISOMAP): {lr_iso_prediction[0]:.1f} g/dL</p>g/dL</p>
+        <p class="hb_prediction">Hb value (DTR): {dtr_prediction[0]:.1f} g/dL</p>
+        <p class="hb_prediction">Hb value (LR-ISOMAP): {lr_iso_prediction[0]:.1f} g/dL</p>
+        <p class="hb_prediction">Hb value (DTR-ISOMAP): {dtr_iso_prediction[0]:.1f} g/dL</p>
         <p class="hb_prediction">Hb value (LR-LLC): {lr_llc_prediction[0]:.1f} g/dL</p>
+        <p class="hb_prediction">Hb value (DTR-LLC): {dtr_llc_prediction[0]:.1f} g/dL</p>
     """, unsafe_allow_html=True)
 else:
     st.write('The sample is empty. Please load a sample with data.')
@@ -120,7 +123,7 @@ else:
 #             <p class="hb_prediction">Hb value (LR): {lr_prediction[0]} g/dL</p>
 #             <p class="hb_prediction">Hb value (DTR): {dtr_prediction[0]:.1f} g/dL</p>
 #             <p class="hb_prediction">Hb value (LR-ISOMAP): {lr_iso_prediction[0]:.1f} g/dL</p>
-            # <p class="hb_prediction">Hb value (DTR-ISOMAP): {dtr_iso_prediction[0]:.1f} g/dL</p>
+#             <p class="hb_prediction">Hb value (DTR-ISOMAP): {dtr_iso_prediction[0]:.1f} g/dL</p>
 #             <p class="hb_prediction">Hb value (LR-LLC): {lr_llc_prediction[0]:.1f} g/dL</p>
 #             <p class="hb_prediction">Hb value (DTR-LLC): {dtr_llc_prediction[0]:.1f} g/dL</p>
 #         """, unsafe_allow_html=True)
