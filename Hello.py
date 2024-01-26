84e07eaa59e693b999e3670a274f27764ceb76ba
hanyswana <146700525+hanyswana@users.noreply.github.com>
2024-01-26 10:22:34 +0800

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -44,7 +44,7 @@ def load_model(model_file):
     return model
     
 lr_model = load_model('pipeline  6.csv_lr_ori.joblib')
-dtr_model = load_model('pipeline  64.csv_dtr_ori.joblib')
+dtr_model = load_model('pipeline  64.csv_dtr_ori2.joblib')
 lr_iso_model = load_model('pipeline  85.csv_lr_iso.joblib')
 dtr_iso_model = load_model('pipeline  63.csv_dtr_iso.joblib')
 lr_llc_model = load_model('pipeline  78.csv_lr_llc.joblib')
