1f668b9e8cfc7d42b41a36c4fc7c5caeed095986
hanyswana <146700525+hanyswana@users.noreply.github.com>
2024-02-26 15:59:10 +0800

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -67,8 +67,8 @@ def main():
     # Define model paths with labels
     model_paths_with_labels = [
         ('Ori (R39)', 'reva-lablink-hb-125-(original-data).csv_r2_0.39_2024-02-15_11-55-27'),
-        ('Normalized Manhattan (R38)', 'reva-lablink-hb-125-(original-data).csv_r2_0.39_2024-02-15_11-55-27'),
-        ('Normalized Manhattan (R40)', 'reva-lablink-hb-125-(original-data).csv_r2_0.39_2024-02-15_11-55-27')
+        ('Normalized Manhattan (R38)', 'lablink-hb-norm-manh.csv_best_model_2024-02-23_00-52-51_r38'),
+        ('Normalized Manhattan (R40)', 'lablink-hb-norm-manh.csv_best_model_2024-02-22_02-09-42_r40')
     ]
 
     # Get data from server (simulated here)
