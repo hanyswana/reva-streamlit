69f1cedaed4a2417ce8b0cb9e93bcb33a1e3ef7d
hanyswana <146700525+hanyswana@users.noreply.github.com>
2024-03-05 10:48:25 +0800

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -67,9 +67,7 @@ def predict_with_model(model, input_data):
 def main():
     # Define model paths with labels
     model_paths_with_labels = [
-        ('Ori (R39)', 'reva-lablink-hb-125-(original-data).csv_r2_0.39_2024-02-15_11-55-27'),
-        ('TFLite1', 'model_new.tflite'),
-        ('TFLite2', 'tflite_model.tflite')
+        ('Ori (R39)', 'reva-lablink-hb-125-(original-data).csv_r2_0.39_2024-02-15_11-55-27')
     ]
     
     # Get data from server (simulated here)
