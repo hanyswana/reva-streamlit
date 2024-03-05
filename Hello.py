c49a0893d4cc396c8a3c68e615a1e5f11f64ce15
hanyswana <146700525+hanyswana@users.noreply.github.com>
2024-03-05 10:58:10 +0800

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -52,44 +52,34 @@ def json_data():
  
     return absorbance_df, wavelengths
 
-def load_model(model_path):
-    # Check if the model is a TensorFlow Lite model based on the file extension
-    if model_path.endswith('.tflite'):
-        # Load TFLite model and allocate tensors.
-        interpreter = tf.lite.Interpreter(model_path=model_path)
+def load_model(model_dir):
+    if model_dir.endswith('.tflite'):  # Check if model is a TensorFlow Lite model
+        # Load TensorFlow Lite model
+        interpreter = tf.lite.Interpreter(model_path=model_dir)
         interpreter.allocate_tensors()
         return interpreter
     else:
-        # Load a regular SavedModel
-        model = tf.saved_model.load(model_path)
+        # Load TensorFlow SavedModel
+        model = tf.saved_model.load(model_dir)
         return model
 
 def predict_with_model(model, input_data):
-    # Check if the model is an instance of the TensorFlow Lite Interpreter
-    if isinstance(model, tf.lite.Interpreter):
+    if isinstance(model, tf.lite.Interpreter):  # Check if model is TensorFlow Lite Interpreter
         input_details = model.get_input_details()
         output_details = model.get_output_details()
-
-        # Assuming input_data is a pandas DataFrame, convert it to numpy array
-        input_array = input_data.to_numpy(dtype='float32') # Adjust dtype if necessary
-        input_array_reshaped = input_array.reshape(input_details[0]['shape'])
         
-        # Set the tensor to point to the input data to be inferred
-        model.set_tensor(input_details[0]['index'], input_array_reshaped)
+        # Assuming input_data is already in the correct shape and type
+        model.set_tensor(input_details[0]['index'], input_data)
         model.invoke()
-
-        # Get the model's output and return it
-        output_data = model.get_tensor(output_details[0]['index'])
-        return output_data
+        predictions = model.get_tensor(output_details[0]['index'])
+        return predictions  # This will be a numpy array
     else:
-        # If it's a regular SavedModel, perform prediction as before
+        # Existing prediction code for TensorFlow SavedModel
         input_array = input_data.to_numpy(dtype='float64')
-        input_array_reshaped = input_array.reshape(-1, 19) # Adjust based on your model's input shape
+        input_array_reshaped = input_array.reshape(-1, 19)  # Adjust to match the number of features your model expects
         input_tensor = tf.convert_to_tensor(input_array_reshaped, dtype=tf.float64)
         predictions = model(input_tensor)
-        return predictions.numpy()
-        
-    return predictions
+        return predictions.numpy()  # Convert predictions to numpy array if needed
 
 def main():
     # Define model paths with labels
