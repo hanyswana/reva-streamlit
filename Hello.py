b473ee14e220edb2070ee2b3b477aae9713a15b5
hanyswana <146700525+hanyswana@users.noreply.github.com>
2024-03-05 10:40:29 +0800

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -68,7 +68,8 @@ def main():
     # Define model paths with labels
     model_paths_with_labels = [
         ('Ori (R39)', 'reva-lablink-hb-125-(original-data).csv_r2_0.39_2024-02-15_11-55-27'),
-        ('TFLite1', 'tflite_model.tflite')
+        ('TFLite1', 'model_new.tflite'),
+        ('TFLite2', 'tflite_model.tflite')
     ]
     
     # Get data from server (simulated here)
