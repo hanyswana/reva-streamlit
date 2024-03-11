24aa1f132022cf159fb8319e8ec1b07408956c80
hanyswana <146700525+hanyswana@users.noreply.github.com>
2024-03-11 09:08:10 +0800

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -87,12 +87,18 @@ def predict_with_model(model, input_data):
         return predictions.numpy()  # Convert predictions to numpy array if needed
     
 def main():
-    # Define model paths with labels
+    # # Define model paths with labels
+    # model_paths_with_labels = [
+    #     ('Ori (R39)', 'reva-lablink-hb-125-(original-data).csv_r2_0.39_2024-02-15_11-55-27'),
+    #     ('TFLite1', 'tflite_model.tflite'),
+    #     ('TFLite2', 'tflite_model4.tflite')
+    # ]
+
+        # Define model paths with labels
     model_paths_with_labels = [
-        ('Ori (R39)', 'reva-lablink-hb-125-(original-data).csv_r2_0.39_2024-02-15_11-55-27'),
-        ('TFLite1', 'tflite_model.tflite'),
-        ('TFLite2', 'tflite_model4.tflite')
+        ('Ori (R39)', 'reva-lablink-hb-125-(original-data).csv_r2_0.39_2024-02-15_11-55-27')
     ]
+    
     
     # Get data from server (simulated here)
     absorbance_data, wavelengths = json_data()
