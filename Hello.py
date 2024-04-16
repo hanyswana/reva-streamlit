f368672136966571403634bb8e58d3a30c351c24
hanyswana <146700525+hanyswana@users.noreply.github.com>
2024-04-16 11:37:08 +0800

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -46,7 +46,7 @@ def snv(input_data):
         
 def json_data():
     # First API call
-    api_url1 = "https://x8ki-letl-twmt.n7.xano.io/api:3Ws6ADLi/bgdata"
+    api_url1 = "https://x8ki-letl-twmt.n7.xano.io/api:U4wk_Gn6/BackgroundReading"
     payload1 = {}
     response1 = requests.get(api_url1, params=payload1)
 
@@ -57,7 +57,7 @@ def json_data():
         return None
 
     # Second API call
-    api_url2 = "https://x8ki-letl-twmt.n7.xano.io/api:Qc5crfn2/spectraldata"
+    api_url2 = "https://x8ki-letl-twmt.n7.xano.io/api:DKaWNKM4/spectral_data"
     payload2 = {}
     response2 = requests.get(api_url2, params=payload2)
 
