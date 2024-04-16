f304841d98a87a2ee7b3e1d66ba60f2c36aa7ea5
hanyswana <146700525+hanyswana@users.noreply.github.com>
2024-04-16 11:09:17 +0800

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -83,7 +83,7 @@ def predict_with_model(model, input_data):
     else:
         # Existing prediction code for TensorFlow SavedModel
         input_array = input_data.to_numpy(dtype='float64')
-        input_array_reshaped = input_array.reshape(-1, 19)  # Adjust to match the number of features your model expects
+        input_array_reshaped = input_array.reshape(-1, 10)  # Adjust to match the number of features your model expects
         input_tensor = tf.convert_to_tensor(input_array_reshaped, dtype=tf.float64)
         predictions = model(input_tensor)
         return predictions.numpy()  # Convert predictions to numpy array if needed
