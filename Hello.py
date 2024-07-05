c7255ff5374cde28045fb1718454cd623f543bcb
hanyswana <146700525+hanyswana@users.noreply.github.com>
2024-07-05 11:24:59 +0800

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -206,7 +206,7 @@ def main():
         
         model = load_model(model_path)
         predictions = predict_with_model(model, prediction_data)
-        predictions_value = predictions[0][0]
+        predictions_value = predictions[0][0] - 1.5
 
         correlation = np.corrcoef(absorbance_all_pp_df.iloc[0], golden_values)[0, 1]
 
